456302296ba3043f50283700e59ad81de48999f1
iyinraphael <iyinraphael@gmail.com>
2020-05-27 18:39:59 -0700

    initial set up

---
+++ Journal/Journal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Journal
+//
+//  Created by Iyin Raphael on 5/27/20.
+//  Copyright © 2020 Iyin Raphael. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
